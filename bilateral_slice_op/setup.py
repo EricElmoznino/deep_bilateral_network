30738c0d66193c05e4ad96876121f649a96f7481
Eric Elmoznino <ericelmoznino@modiface.com>
2019-03-05 13:46:21 -0500

    Changed the name of the bilateral_slice compilation folder so that imports are simpler

--- bilateral_slice_op/setup.py
+++ bilateral_slice_op/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 
-setup(name='bilateral_slice',
+setup(name='bilateral_slice_op',
       ext_modules=[
           CUDAExtension('bilateral_slice_cuda', [
               'bilateral_slice_cuda.cpp',
